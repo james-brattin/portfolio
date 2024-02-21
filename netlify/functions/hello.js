8a8f84e697c64158086343f4bda257f4e61add58
James Brattin <james.brattin@gmail.com>
2024-02-21 05:04:37 +0100

    added random gif to homepage

--- netlify/functions/hello.js
+++ netlify/functions/hello.js
@@ -1,14 +1,15 @@
 exports.handler = async (event, context) => {
   const url = `https://api.giphy.com/v1/gifs/random?api_key=${process.env.GIPHY_API_KEY}&tag=funny&rating=g`;
 
-  fetch(url)
-    .then((response) => {
-      return response.json();
-    })
-    .catch((error) => {
-      return {
-        statusCode: 500,
-        body: JSON.stringify({ message: "An error occurred" })
-      }
-    });
+  try {
+    const res = await fetch(url);
+    const data = await res.json();
+    return { statusCode: 200, body: JSON.stringify({ data }) };
+  } catch (error) {
+    console.log(error);
+    return {
+      statusCode: 500,
+      body: JSON.stringify({ error: "Failed fetching data" }),
+    };
+  }
 }
--- public/main.js
+++ public/main.js
@@ -1,4 +1,17 @@
-fetch('https://jamesbrattin.dev/.netlify/functions/hello')
-  .then(response => response.json())
-  .then(data => console.log(data))
-  .catch(error => console.error('Error:', error));
+document.addEventListener('DOMContentLoaded', () => {
+  const newDiv = document.createElement("div");
+
+  fetch(`${document.location}.netlify/functions/hello`)
+    .then(response => response.json())
+    .then(data => {
+      const width = data.data.data.images.original.width;
+      const height = data.data.data.images.original.height;
+      const url = data.data.data.images.original.url;
+      const alt = data.data.data.title;
+      newDiv.innerHTML = `
+        <center><img width="${width}" height="${height}" src="${url}" alt="${alt}"></center>
+      `;
+      document.body.appendChild(newDiv);
+    })
+    .catch(error => console.error('Error:', error));
+});
