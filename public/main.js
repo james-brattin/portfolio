ba168477f2df622cb23021add0a4fce7945cff9c
James Brattin <james.brattin@gmail.com>
2024-03-05 20:48:37 +0100

    unregistering service worker for now

--- public/main.js
+++ public/main.js
@@ -1,7 +1,7 @@
 document.addEventListener('DOMContentLoaded', () => {
-  if ('serviceWorker' in navigator) {
-    navigator.serviceWorker.register('service-worker.js');
-  }
+  // if ('serviceWorker' in navigator) {
+  //   navigator.serviceWorker.register('service-worker.js');
+  // }
 
   const newDiv = document.createElement("div");
 
