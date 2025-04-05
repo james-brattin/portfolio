903e8504aa3f2c15399836975722e47ef1c70e74
James Brattin <james.brattin@tamu.edu>
2025-04-05 00:57:53 -0400

    removing call to api

--- main.js
+++ main.js
@@ -3,20 +3,3 @@
 //     navigator.serviceWorker.register('service-worker.js');
 //   }
 // });
-
-//   const newDiv = document.createElement("div");
-
-//   fetch(`${document.location}.netlify/functions/hello`)
-//     .then(response => response.json())
-//     .then(data => {
-//       const width = data.data.data.images.original.width;
-//       const height = data.data.data.images.original.height;
-//       const url = data.data.data.images.original.url;
-//       const alt = data.data.data.title;
-//       newDiv.innerHTML = `
-//         <center><img width="${width}" height="${height}" src="${url}" alt="${alt}"></center>
-//       `;
-//       document.body.appendChild(newDiv);
-//     })
-//     .catch(error => console.error('Error:', error));
-// });
